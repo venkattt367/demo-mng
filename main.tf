8298d77ac3c9211c64bd7cf2bfdcd564501b41d9
demoevg <demoevgone@gmail.com>
2022-02-20 13:12:38 +0530

    sASA

--- main.tf
+++ main.tf
@@ -1,5 +1,24 @@
-resource "azurerm_resource_group" "example" {
-  name     = "my-example-rg"
-  location = "West Europe"
-}
 
+variable "name"{
+  default = ""
+  }
+resource "azurerm_resource_group" "test" {
+  name     = "${var.name}"
+  location = "West US"
+
+  tags = {
+    environment = "Production"
+  }
+}
+resource "azurerm_storage_account" "test" {
+  name                     = "demoforusers"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+    KEY = "VALUE"
+  }
+}
